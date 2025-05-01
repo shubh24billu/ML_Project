037412c2c43f89eb7597a158d5f7b6acfa41306b
Shubham <shubham2swarnakar@gmail.com>
2025-05-01 14:31:55 +0530

    new commit

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
-
+import logging
+from logger import logging
 
 def error_message_detail(error, error_detail: sys):
     """
@@ -10,3 +11,16 @@ def error_message_detail(error, error_detail: sys):
     line_number = exc_tb.tb_lineno
     error_message = f"Error occurred in script: [{file_name}] at line number: [{line_number}] error message: [{str(error)}]"
     return error_message
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail: sys):   
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail=error_detail)
+    def __str__(self):
+        return self.error_message
+
+# if __name__ == "__main__":
+#     try:
+#         a = 1 / 0
+#     except Exception as e:
+#         logger = logging.info("Logging has been set up.")
+#         raise CustomException(e, sys) from e
